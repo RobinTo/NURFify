48418f6771d87c572b8b8e9b37d4e71df74404ab
Robin Tollisen <al4crity@gmail.com>
2015-04-08 11:56:28 +0200

    Added a basic frontpage with some information.

--- js/application/app.js
+++ js/application/app.js
@@ -6,7 +6,6 @@ var app = (function(){
         for (var t in allchampions){
             (function(){
                 var c = allchampions[t];
-                console.log(c);
                 imagehandler.getImage(config.ddragonbase + config.getVersion() + '/img/champion/' + c.image.full, function(image){
                     image.addClass('icon');
                     image.attr('champion', c.name);
@@ -40,13 +39,7 @@ var app = (function(){
                 championArray.push(cobject);
             }
 
-            var killers = championArray.sort(function(a,b){
-                return (b.killsInWins+ b.killsInLosses)/(b.wins+ b.losses) - (a.killsInWins+ a.killsInLosses)/(a.wins+ a.losses)
-            });
-            for (var k = 0; k < killers.length; k++){
-                console.log(staticdatahandler.getChampion(killers[k].championId).name);
-                console.log((killers[k].killsInWins+ killers[k].killsInLosses)/(killers[k].wins+killers[k].losses))
-            }
+            createFrontPage();
         });
 
         $('.championFilter').on('keyup', function(event){
@@ -77,7 +70,63 @@ var app = (function(){
         })
     }
 
+    function createFrontPage(){
+        var $c = $('.championContent');
+        $c.empty();
+
+        var killers = championArray.slice(0).sort(function(a,b){
+            return (b.killsInWins+ b.killsInLosses)/(b.wins+ b.losses) - (a.killsInWins+ a.killsInLosses)/(a.wins+ a.losses)
+        });
+        var assisters = championArray.slice(0).sort(function(a,b){
+            return (b.assistsInWins+ b.assistsInLosses)/(b.wins+ b.losses) - (a.assistsInWins+ a.assistsInLosses)/(a.wins+ a.losses)
+        });
+        var warders = championArray.slice(0).sort(function(a,b){
+            return (b.wardsPlacedInWins+ b.wardsPlacedInLosses)/(b.wins+ b.losses) - (a.wardsPlacedInWins+ a.wardsPlacedInLosses)/(a.wins+ a.losses)
+        });
+        var diers = championArray.slice(0).sort(function(a,b){
+            return (b.deathsInWins+ b.deathsInLosses)/(b.wins+ b.losses) - (a.deathsInWins+ a.deathsInLosses)/(a.wins+ a.losses)
+        });
+
+        var $table = $('<table class="frontpageTable"></table>')
+        var $tr = $('<tr></tr>');
+        $tr.append('<td class="killsHeader">Kills</td>');
+        $tr.append('<td class="deathsHeader">Deaths</td>');
+        $tr.append('<td class="assistsHeader">Assists</td>');
+        $tr.append('<td class="wardsHeader">Wards</td>');
+        $table.append($tr);
+        var allChampions = staticdatahandler.getAllChampions();
+        for(var i = 0; i < killers.length && i < 5; i++){
+            (function(){
+                var $tr = $('<tr></tr>');
+                $tr.append('<td class="kills"></td>');
+                $tr.append('<td class="deaths"></td>');
+                $tr.append('<td class="assists"></td>');
+                $tr.append('<td class="wards"></td>');
+
+                var kpg = (killers[i].killsInWins+ killers[i].killsInLosses)/(killers[i].wins+ killers[i].losses),
+                    dpg = (diers[i].deathsInWins+ diers[i].deathsInLosses)/(diers[i].wins+ diers[i].losses),
+                    apg = (assisters[i].assistsInWins+ assisters[i].assistsInLosses)/(assisters[i].wins+ assisters[i].losses),
+                    wpg = (warders[i].wardsPlacedInWins+ warders[i].wardsPlacedInLosses)/(warders[i].wins+ warders[i].losses);
+                imagehandler.getImage(config.ddragonbase + config.getVersion() + '/img/champion/' + allChampions[killers[i].championId].image.full, function(image){
+                    $tr.find('.kills').append(image).append('<br/>'+kpg.toFixed(2));
+                });
+                imagehandler.getImage(config.ddragonbase + config.getVersion() + '/img/champion/' + allChampions[assisters[i].championId].image.full, function(image){
+                    $tr.find('.assists').append(image).append('<br/>'+apg.toFixed(2));
+                });
+                imagehandler.getImage(config.ddragonbase + config.getVersion() + '/img/champion/' + allChampions[diers[i].championId].image.full, function(image){
+                    $tr.find('.deaths').append(image).append('<br/>'+dpg.toFixed(2));
+                });
+                imagehandler.getImage(config.ddragonbase + config.getVersion() + '/img/champion/' + allChampions[warders[i].championId].image.full, function(image){
+                    $tr.find('.wards').append(image).append('<br/>'+wpg.toFixed(2));
+                });
+                $table.append($tr);
+            })();
+        }
+        $c.append($table);
+    }
+
     return{
-        init : init
+        init : init,
+        createFrontPage : createFrontPage
     }
 })();
